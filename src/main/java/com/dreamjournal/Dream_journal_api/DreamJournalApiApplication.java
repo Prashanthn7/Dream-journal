9f8446716a193fc6df1a4a898859daff789ed433
Prashanthn7 <pn342604gmail.com>
2025-03-03 19:54:09 +0530

    Updating git ignore file

---
+++ src/main/java/com/dreamjournal/Dream_journal_api/DreamJournalApiApplication.java
@@ -0,0 +1,13 @@
+package com.dreamjournal.Dream_journal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamJournalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamJournalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamjournal/Dream_journal_api/DreamJournalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamjournal.Dream_journal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamJournalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
